09ce0bf7e407ad2f17edacd6cc4f376b393f13da
Jen Pirrone <pirronej104@gmail.com>
2018-11-26 10:06:37 -0500

    Game Skeleton

---
+++ assets/javascript/game.js
@@ -0,0 +1,59 @@
+//Ready the DOM on page load
+
+$(document).ready(function() {
+//Objects holding the players
+
+var player1 = {
+    Name: "Player 1",
+    Health: 140,
+    Damage: 10,
+
+};
+
+var player2 = {
+    Name: "Player 2",
+    Health: 150,
+    Damage: 8,
+};
+
+var player3 = {
+    Name: "Player 3",
+    Health: 175,
+    Damage: 5,
+};
+
+var player4 = {
+    Name: "Player 4",
+    Health: 130,
+    Damage: 12,
+};
+
+
+}
+
+
+//Game variables to hold the attack, the player position, and function names
+
+//Start Game function 
+
+
+
+//Select a player function 
+
+//place the player in the battle area
+
+//select an opponent function
+
+//place the opponent in the battle area
+
+//calculate damage 
+
+//display attack status message
+
+//display win or lose message 
+
+//reset game function 
+
+//update HTML function 
+
+//onclick events 

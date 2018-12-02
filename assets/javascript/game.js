645604a2cea5db3e5a9970a1108f7e02dbbaf9b4
Jen Pirrone <pirronej104@gmail.com>
2018-12-01 21:03:54 -0500

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,7 +19,7 @@ var wins = 0;
                 Id: 0,
                 Name: "Automation",
                 Health: 150,
-                scourgeDamage: 15,
+                scourgeDamage: 20,
                 Damage: 5,
                 Image: "assets/images/robot.png",
                 ScourgePower: "Layoffs",
@@ -31,7 +31,7 @@ var wins = 0;
                 Id: 1,
                 Name: "Consumerism",
                 Health: 130,
-                scourgeDamage: 12,
+                scourgeDamage: 25,
                 Damage: 7,
                 Image: "assets/images/kitty.png",
                 ScourgePower: "Crushing Debt",
@@ -42,7 +42,7 @@ var wins = 0;
                 Id: 2,
                 Name: "Social Media",
                 Health: 140,
-                scourgeDamage: 8,
+                scourgeDamage: 15,
                 Damage: 3,
                 Image: "assets/images/wolf.png",
                 ScourgePower: "FOMO",
@@ -53,8 +53,8 @@ var wins = 0;
                 Id: 3,
                 Name: "Climate Change",
                 Health: 175,
-                scourgeDamage: 20,
-                Damage: 9,
+                scourgeDamage: 30,
+                Damage: 7,
                 Image: "assets/images/bunny.jpg",
                 ScourgePower: "Catastrophic Weather",
                 WeaponPower: "Rising Sea Levels",
@@ -70,9 +70,12 @@ var wins = 0;
             //clear all game play DIVs
             $("#yourPlayer").empty();
             $("#yourOpponent").empty();
-            $("#enemies").empty();
+            $("#availablePlayers").empty();
+            $("#yourPlayerMessage").empty();
+            $("#yourOpponentMessage").empty();
+            $("#restart").hide();
             // $("#earth").hide();
-            // $("#game").show();
+            $("#game").show();
 
             // function addPlayers() {
                 $.each(availablePlayers, function(index, yourPlayer) {
@@ -93,6 +96,8 @@ var wins = 0;
                     wins = $("#defeated div").length;
                     console.log(wins)
                     if(wins === 12) {
+                        $("#game").hide();
+                        $("#restart").show();
                         alert("You've won the game!");
                         // $("#game").hide();
                         // $("#earth").show();
@@ -105,25 +110,22 @@ var wins = 0;
 
                             //update health for your Weapon
                             yourPlayer.Health -= yourOpponent.scourgeDamage;
-
                             //update health on DOM
                             $("#yourPlayer").find(".panel-footer").html("Health: " + yourPlayer.Health);
+                            //update status and attack message
+                            $("#yourOpponentMessage").html("You used " + yourPlayer.WeaponPower + " for " + yourPlayer.Damage + " damage ");
 
-                             //update status and attack message
-                             $("#yourOpponentMessage").html("You used " + yourPlayer.WeaponPower + " for " + yourPlayer.Damage + " damage ");
-
-                                    //update health for your opponent
-                                    yourOpponent.Health -= yourPlayer.Damage;
-                                    // yourPlayer.Damage += yourPlayer.Damage;
-                                    
-                                    //update health on the DOM 
-                                    $("#yourOpponent").find(".panel-footer").html("Health: " + yourOpponent.Health);                                
-                                
-                                    //update status and attack messages
-                                    $("#yourPlayerMessage").html(yourOpponent.Name + " used " + yourOpponent.ScourgePower + " for " + yourOpponent.scourgeDamage + " damage ");
+                            //update health for your opponent
+                            yourOpponent.Health -= yourPlayer.Damage;
+                            //update health on the DOM 
+                            $("#yourOpponent").find(".panel-footer").html("Health: " + yourOpponent.Health);
+                            //update status and attack messages
+                            $("#yourPlayerMessage").html(yourOpponent.Name + " used " + yourOpponent.ScourgePower + " for " + yourOpponent.scourgeDamage + " damage ");
 
                                     //update your attack damage for next hit
                                     yourPlayer.Damage += yourPlayer.Damage;
+                                    console.log(yourPlayer.Health);
+                                    console.log(yourOpponent.Health);
 
                                 } else if (yourOpponent.Health <= 0 && yourPlayer.Health > 0) {
                                     console.log(yourOpponent);
@@ -137,8 +139,9 @@ var wins = 0;
                                 }
                             if(yourPlayer.Health <= 0) {
                                 $("#game").hide();
+                                $("#restart").show();
                                 alert("Oh No....you lose!");
-                                $("#earth").show();
+                                
                                 
 
                             }
@@ -207,7 +210,19 @@ var wins = 0;
             });
 
             $(document).on("click", "#fightbutton", function() {
+                // if((yourPlayer !== null && yourPlayer.Health > 0) && (yourOpponent !== null && yourOpponent.Health > 0)){
+                //     fight(); 
+                // }
                 fight();
+               
+            });
+
+            $(document).on("click", "#restart", function() {
+                // if((yourPlayer !== null && yourPlayer.Health > 0) && (yourOpponent !== null && yourOpponent.Health > 0)){
+                //     fight(); 
+                // }
+                startGame();
+               
             });
 
 
